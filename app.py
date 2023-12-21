b9c3ff9c035747b422ae4cddba6b7d75e3f221bf
李想 <>
2023-12-21 20:30:30 +0800

    as3

--- app.py
+++ app.py
@@ -1,127 +1,155 @@
-import csv
-from collections import Counter
+from collections import defaultdict
 
-from flask import Flask, render_template, jsonify, request
+from flask import Flask, request, jsonify, render_template
+import math
+import time
+import nltk
+from nltk.internals import Counter
+from nltk.tokenize import word_tokenize
+
+from pydocumentdb import document_client
 
 app = Flask(__name__)
+ENDPOINT = "https://tutorial-uta-cse6332.documents.azure.com:443/"
+MASTERKEY = "fSDt8pk5P1EH0NlvfiolgZF332ILOkKhMdLY6iMS2yjVqdpWx4XtnVgBoJBCBaHA8PIHnAbFY4N9ACDbMdwaEw=="
+DATABASE_ID = "tutorial"
+COLLECTION_ID1 = "us_cities"
+COLLECTION_ID2 = "reviews"
+client = document_client.DocumentClient(ENDPOINT, {'masterKey': MASTERKEY})
 
 
-# 这是读取csv文件的方法
-def read_csv():
-    csv_data = []
-    with open('static/amazon-reviews.csv', 'r') as f:
-        csv_file = csv.DictReader(f)
-        for data in csv_file:
-            csv_data.append(data)
-    return csv_data
+def get_cities_data():
+    sql = "SELECT c.city, c.lat, c.lng FROM c"
+    o = {"enableCrossPartitionQuery": True}
+    r = list(client.QueryDocuments(f"dbs/{DATABASE_ID}/colls/{COLLECTION_ID1}", sql, o))
+    c = []
+    for i in r:
+        c.append({
+            "city": i['city'],
+            "lat": i['lat'],
+            "lng": i['lng']
+        })
+    return c
 
 
-# 这是读取城市信息的方法
-def read_city():
-    city_data = []
-    with open('static/us-cities.csv', 'r') as f:
-        csv_file = csv.DictReader(f)
-        for data in csv_file:
-            city_data.append(data)
-    return city_data
+# Helper function to calculate Eular distance
+def calculate_eular_distance(lat1, lng1, lat2, lng2):
+    return math.sqrt((lat1 - lat2) ** 2 + (lng1 - lng2) ** 2)
 
 
-# 这是写入csv文件的方法
-def write_csv(filename, data):
-    with open(filename, 'w', newline='', encoding='utf-8') as f:
-        writer = csv.DictWriter(f, fieldnames=data[0].keys())
-        writer.writeheader()
-        writer.writerows(data)
+# Route to handle closest cities query
+@app.route('/stat/closest_cities', methods=['GET'])
+def closest_cities():
+    start_time = time.time()
 
-@app.route('/word')
-def word():
-    return render_template('word.html')
+    city_name = request.args.get('city')
+    page_size = int(request.args.get('page_size', 50))
+    page = int(request.args.get('page', 0))
 
-@app.route('/popular_words', methods=['GET'])
-def popular_words():
-    # 读取城市信息
-    city_data = read_city()
-    # 读取评论数据
-    reviews_data = read_csv()
-    city = request.args.get('city')
-    limit = request.args.get('limit', type=int, default=10)
+    cities_data = get_cities_data()
 
-    # 筛选符合城市条件的评论
-    if city:
-        filtered_reviews = [review for review in reviews_data if review['city'] == city]
-    else:
-        filtered_reviews = reviews_data
+    # Find the requested city's coordinates
+    requested_city = next((city for city in cities_data if city['city'].lower() == city_name.lower()), None)
+    if not requested_city:
+        return jsonify({"error": "City not found"}), 404
 
-    # 统计单词出现频率
-    word_counts = Counter()
-    for review in filtered_reviews:
-        words = review['review'].split()
-        word_counts.update(words)
+    # Calculate distances
+    distances = []
+    for city in cities_data:
+        if city['city'].lower() != city_name.lower():
+            distance = calculate_eular_distance(float(requested_city['lat']), float(requested_city['lng']),
+                                                float(city['lat']), float(city['lng']))
+            distances.append((city['city'], distance))
 
-    # 获取最常见的单词
-    common_words = word_counts.most_common(limit)
+    # Sort by distance
+    sorted_cities = sorted(distances, key=lambda x: x[1])
 
-    # 构建响应
-    response = [{"term": word, "popularity": count} for word, count in common_words]
+    # Implement pagination
+    start = page * page_size
+    end = start + page_size
+    paginated_cities = sorted_cities[start:end]
+
+    # Compute response time
+    elapsed_time = (time.time() - start_time) * 1000  # Time in milliseconds
+
+    return jsonify({
+        "closest_cities": paginated_cities,
+        "time_ms": elapsed_time
+    })
+
+
+@app.route('/as3', methods=['GET'])
+def stat():
+    return render_template('as3.html')
+
+
+def get_reviews_data():
+    sql = "SELECT c.city, c.review FROM c"
+    o = {"enableCrossPartitionQuery": True}  # 如果集合是分区集合，需要启用跨分区查询
+    # 执行查询
+    q = list(client.QueryDocuments(f"dbs/{DATABASE_ID}/colls/{COLLECTION_ID2}", sql, o))
+    d = []
+    for i in q:
+        d.append({
+            "city": i['city'],
+            "review": i['review']
+        })
+    return d
+
+
+# 从文件读取停用词
+def load_stopwords(file_path):
+    with open(file_path, 'r', encoding='utf-8') as file:
+        stopwords = set(file.read().splitlines())
+    return stopwords
+
+
+stop_words = load_stopwords('./static/stopwords.txt')
+
+
+# 辅助函数：从评论中提取词汇
+def extract_words(reviews):
+    words = []
+    for review in reviews:
+        word_tokens = word_tokenize(review.lower())
+        filtered_words = [word for word in word_tokens if word.isalpha() and word not in stop_words]
+        words.extend(filtered_words)
+    return words
+
+
+@app.route('/stat/knn_reviews', methods=['GET'])
+def knn_reviews():
+    start_time = time.time()
+
+    # 从请求中获取参数
+    classes = int(request.args.get('classes', 6))
+    k = int(request.args.get('k', 3))
+    words_count = int(request.args.get('words', 100))
+
+    # 从数据库获取评论数据
+    reviews_data = get_reviews_data()
+    class_vectors = {}
+    for class_id in range(classes):
+        class_reviews = [review['review'] for review in reviews_data if review['class_id'] == class_id]
+        class_vectors[class_id] = extract_words(class_reviews)
+    elapsed_time = (time.time() - start_time) * 1000
+    class_words = defaultdict(list)
+    for class_id in range(classes):
+        class_reviews = [review['review'] for review in reviews_data if review['class_id'] == class_id]
+        all_words = extract_words(class_reviews)
+        word_freq = Counter(all_words)
+        most_common = word_freq.most_common(words_count)
+        class_words[class_id] = most_common
+    # 计算响应时间
+    elapsed_time = (time.time() - start_time) * 1000
+    # 构建并返回 JSON 响应
+    response = {
+        "class_words": class_words,
+        "time_ms": elapsed_time
+    }
+
     return jsonify(response)
-
-
-@app.route('/popular_words2', methods=['GET'])
-def popular_words2():
-    # 读取城市信息
-    city_data = read_city()
-    # 读取评论数据
-    reviews_data = read_csv()
-    city = request.args.get('city')
-    limit = request.args.get('limit', type=int, default=10)
-    city_population = {city['city']: int(city['population']) for city in city_data}
-
-    # 筛选符合城市条件的评论
-    if city:
-        filtered_reviews = [review for review in reviews_data if review['city'] == city]
-    else:
-        filtered_reviews = reviews_data
-
-    # 计算单词热度
-    word_popularity = {}
-    for review in filtered_reviews:
-        words = review['review'].split()
-        review_city = review['city']
-        for word in words:
-            if word not in word_popularity:
-                word_popularity[word] = {'cities': set(), 'popularity': 0}
-            if review_city not in word_popularity[word]['cities']:
-                word_popularity[word]['cities'].add(review_city)
-                word_popularity[word]['popularity'] += city_population.get(review_city, 0)
-
-    # 获取最受欢迎的单词
-    popular_words = sorted(word_popularity.items(), key=lambda x: x[1]['popularity'], reverse=True)
-    popular_words = popular_words[:limit]
-
-    # 构建响应
-    response = [{"term": word, "popularity": info['popularity']} for word, info in popular_words]
-    return jsonify(response)
-
-
-# 替换单词
-@app.route('/substitute_words', methods=['POST'])
-def substitute_words():
-    request_data = request.get_json()
-    original_word = request_data.get('word')
-    new_word = request_data.get('substitute')
-
-    reviews_data = read_csv()
-    affected_reviews = 0
-
-    for review in reviews_data:
-        if original_word in review['review']:
-            review['review'] = review['review'].replace(original_word, new_word)
-            affected_reviews += 1
-
-    write_csv('static/amazon-reviews.csv', reviews_data)
-
-    return jsonify({"affected_reviews": affected_reviews})
 
 
 if __name__ == '__main__':
-    app.run()
+    app.run(debug=True)
